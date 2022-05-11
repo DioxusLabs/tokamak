425ca0c1400273cef07483db25c6aa07469ae734
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-05-11 14:37:44 -0400

    feat: add more to ws

--- examples/sqlx.rs
+++ examples/sqlx.rs
@@ -1,37 +1,16 @@
-use std::sync::Arc;
+use sqlx::SqlitePool;
 
-use sqlx::{Row, SqlitePool};
-use tokamak::{Request, Responder, Response, Result};
+use std::sync::Arc;
+use tokamak::{
+    ws::{WebSocketReceiver, WebSocketSender},
+    App, Request, Responder, Result,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut app = tokamak::new(sqlx::SqlitePool::connect("DATABASE_URL").await?);
 
-    app.at("/dogs")
-        .get(|req: Request<SqlitePool>| async move {
-            let dogs = sqlx::query("SELECT * FROM todos")
-                .fetch_all(req.state())
-                .await
-                .unwrap();
-
-            ""
-        })
-        .put(|req: Request<SqlitePool>| async move {
-            // Insert the task, then obtain the ID of this row
-            let id = sqlx::query(r#" INSERT INTO todos ( description ) VALUES ( ?1 ) "#)
-                .execute(&mut req.state().acquire().await.unwrap())
-                .await
-                .unwrap()
-                .last_insert_rowid();
-
-            ""
-        })
-        .delete(delete_todo)
-        .ws(|state, rx, tx| async move {
-            //
-
-            Ok(())
-        });
+    app.at("/dogs").get(delete_todo).ws(todo_ws);
 
     app.listen("127.0.0.1").await?;
 
@@ -47,4 +26,18 @@ async fn delete_todo(req: Request<SqlitePool>) -> impl Responder {
         .last_insert_rowid();
 
     "".to_string()
+}
+
+async fn todo_ws(
+    state: Arc<App<SqlitePool>>,
+    mut tx: WebSocketSender,
+    mut rx: WebSocketReceiver,
+) -> Result<()> {
+    tx.send(tokamak::Message::Text("Hello".to_string())).await?;
+
+    while let Ok(Some(msg)) = rx.recv().await {
+        // Do something with the message
+    }
+
+    Ok(())
 }
